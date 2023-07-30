0ff061631e9e500dd0b34b09a9207b994fa6e581
JackWH <yukiakira259@gmail.com>
2023-07-30 23:45:08 +0700

    Added things

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=course3-uadicity-single-2.c7q4rf3mnsry.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::course3-udacity-bucket
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=udacity
 export JWT_SECRET=testing
 export URL=http://localhost:8100
